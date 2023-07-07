9e464bd194c71412108e3016ee1a73c7e6068b07
Elvis Njau <elvisbater@gmail.com>
2023-07-07 09:55:01 -0300

    fetched animals from the URL

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const animalDetailsContainer = document.getElementById("animal-details");
 const voteButton = document.getElementById("vote-button");
 const resetButton = document.getElementById("reset-button");
 const addAnimalForm = document.getElementById("add-animal-form");
+
+// Fetch animals from the URL
+async function fetchAnimals() {
+    try {
+      const response = await fetch(CHARACTERS_URL);
+      const data = await response.json();
+      renderAnimals(data);
+    } catch (error) {
+      console.log("Error fetching animals:", error);
+    }
+  }
+  
